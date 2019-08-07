7104a5727b1196c514c2279bfff3dc9e37512f7e
Marcos Barbosa <marcosBarbosa@MarcosBarbosa.local>
2019-08-07 11:47:18 -0300

    correct

--- src/main/java/Teste.java
+++ src/main/java/Teste.java
@@ -2,9 +2,10 @@ public class Teste {
 
 
     public static void main(String[] args){
-        Carro fusca = new Carro("Azul", "Fusca66", 90);
+        Carro fusca = new Carro();
         if (fusca.ligaedesliga) {
-            fusca.acelera(60); 
+
+            fusca.acelera(60);
         }
     }
 }
